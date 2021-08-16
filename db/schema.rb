1bb6069d10c28fb42bc2cc24b178c661470eb142
Tristan Monteiro <71944372+Git-TM@users.noreply.github.com>
2021-08-16 15:40:03 +0200

    Generated the model booking

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_16_132828) do
+ActiveRecord::Schema.define(version: 2021_08_16_133834) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,17 @@ ActiveRecord::Schema.define(version: 2021_08_16_132828) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_artworks_on_user_id"
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.bigint "artwork_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["artwork_id"], name: "index_bookings_on_artwork_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,4 +53,6 @@ ActiveRecord::Schema.define(version: 2021_08_16_132828) do
   end
 
   add_foreign_key "artworks", "users"
+  add_foreign_key "bookings", "artworks"
+  add_foreign_key "bookings", "users"
 end
