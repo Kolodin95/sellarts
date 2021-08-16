d1e8753f447d61b183b7362fc2ec2b7600206e41
axel burtz <axelburtz@gmail.com>
2021-08-16 16:27:00 +0200

    generate controller artworks index and show

---
+++ app/controllers/artworks_controller.rb
@@ -0,0 +1,30 @@
+class ArtworksController < ApplicationController
+
+  def index
+    @artworks = Artwork.all
+  end
+
+  def show
+    @artworks = Artwork.find(params[:id])
+  end
+
+  def new
+    @artworks = Artwork.new
+  end
+
+  def create
+    @artworks = Artwork.new(params_artworks)
+    @artworks.save
+    if @artworks.save
+      redirect_to artwork_path(@artworks)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def params_artworks
+    params.require(:artworks).permit(:title, :author, :creation_date, :denomination, :price)
+  end
+end
