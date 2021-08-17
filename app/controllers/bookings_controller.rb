cf09ee110c0671f8e9774a258b0c86624bb883b5
jean thomassin <jean_thomassin@orange.fr>
2021-08-17 11:35:29 +0200

    modification on bookings controller + view new

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,14 +1,17 @@
 class BookingsController < ApplicationController
   def new
     @booking = Booking.new
+    authorize @booking
   end
 
   def create
     @booking = Booking.new(params_bookings)
+    authorize @booking
     if booking.save 
       # redirect_to pages_dashboard_path() voir avec le pro des dashboard
     else  
       render :new
+    end  
   end  
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     resources :artworks, only: %i[index show new create]
     resources :pages, only: %i[dashboard]
+    resources :bookings, only: %i[new create]
 end
