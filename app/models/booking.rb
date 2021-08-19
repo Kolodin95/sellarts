239678ab7f7a0f4274ad14c3c2650f2c5335412d
Tristan Monteiro <71944372+Git-TM@users.noreply.github.com>
2021-08-19 11:20:38 +0200

    Merge conflict

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,13 @@
 class Booking < ApplicationRecord
   belongs_to :artwork
   belongs_to :user
+
+
+  def total_price
+    raise
+    # Je récupère le start et end date
+    # Je récupère le prix de l'oeuvre au jour
+    # Je calcule la différence entre mes 2 dates après harmonisation
+    # Je return dans une variable d'instance le date diff * prix
+  end
 end
