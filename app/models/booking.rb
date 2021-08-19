30d07701003257bed5d8b0b2fda7ef888c58d1d8
Tristan Monteiro <71944372+Git-TM@users.noreply.github.com>
2021-08-19 12:10:43 +0200

    I added the total price on the dashbord card, and icon style

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,12 +2,9 @@ class Booking < ApplicationRecord
   belongs_to :artwork
   belongs_to :user
 
-
   def total_price
-    raise
-    # Je récupère le start et end date
-    # Je récupère le prix de l'oeuvre au jour
-    # Je calcule la différence entre mes 2 dates après harmonisation
-    # Je return dans une variable d'instance le date diff * prix
+    diff_date = end_date - start_date
+    calculated_price = (artwork.price * diff_date).to_i
+    return "#{calculated_price}"
   end
 end
